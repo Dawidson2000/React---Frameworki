13e563c9f0bb99d13b59c83ff84aba6f9a9c87ff
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-04-04 18:45:52 +0200

    Add LeftMenu

---
+++ my-app/src/components/LeftMenu/LeftMenu.tsx
@@ -0,0 +1,118 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+import {Colors} from '../../styledHelpers/Colors'
+import {fontSize} from '../../styledHelpers/FontSizes';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: ${fontSize[12]};
+`;
+
+const TopCard = styled.div`
+    width: 210px;
+    height: 220px;
+    background-color: ${Colors.white};
+    margin: 20px;
+    border-radius: 5px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: flex-end;
+`;
+
+const BottonIcons = styled.div`
+    width: 100%;
+    height: 70px;
+    border-top: solid 2px #e1e3e6;
+`;
+
+const Panel = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50%;
+`;
+
+const CustomIconTop = styled.img`
+    width: 20px;
+    height: 20px;
+    color: black;
+    margin: 0 10px;
+`;
+
+const CustomIconBotton = styled.img`
+    width: 25px;
+    height: 25px;
+    color: black;
+    margin: 10px 15px;
+`;
+
+const RightButton = styled.button`
+    height: 20px;
+    width: 30px;
+    margin-left: 5px;
+    margin-right: 15px;
+    background-color: ${Colors.white};
+    border: solid 1px;
+    border-radius: 3px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);
+`;
+
+const InnerWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const BottonCard = styled.div`
+    margin: 0 20px 20px 20px;
+    width: 210px;
+`;
+
+export const LeftMenu: FC = () => {
+    return(
+        <Wrapper>
+            <TopCard>
+                <BottonIcons>
+                <Panel>
+                    <InnerWrapper>
+                        <CustomIconTop src='../../media/icons/network.png' />
+                        <div>Your network</div>
+                    </InnerWrapper>
+                    <RightButton>
+                        <img src='../../media/icons/user-plus.png'/>    
+                    </RightButton>
+                </Panel>
+                <Panel>
+                    <InnerWrapper>
+                        <CustomIconTop src='../../media/icons/publications.png' />
+                        <div>Your Publications</div>
+                    </InnerWrapper>
+                    <RightButton>
+                        <img src='../../media/icons/plus.png'/>    
+                    </RightButton>
+                </Panel>
+                </BottonIcons>
+            </TopCard>
+            
+            <BottonCard>
+                <InnerWrapper>
+                    <CustomIconBotton src='../../media/icons/publications.png' />
+                    <div>Publications</div>
+                </InnerWrapper>
+                <InnerWrapper>
+                    <CustomIconBotton src='../../media/icons/ecosystem.png' />
+                    <div>Ecosystem</div>
+                </InnerWrapper> 
+                <InnerWrapper>
+                    <CustomIconBotton src='../../media/icons/entities2.png' />
+                    <div>Entities</div>
+                </InnerWrapper>   
+            </BottonCard>
+        </Wrapper>
+    )
+}
--- my-app/src/components/MainPage/MainPage.tsx
+++ my-app/src/components/MainPage/MainPage.tsx
@@ -2,14 +2,15 @@ import React, {FC} from 'react';
 import styled from 'styled-components';
 
 import {TopBar} from '../TopBar/TopBar';
+import {LeftMenu} from '../LeftMenu/LeftMenu';
 
 const Wrap = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: lightgrey;
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-    font-family: 'Open Sans', sans-serif;
-    font-weight: 500;
+    background-color: #f5f7f9;
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Poppins:wght@500&display=swap');
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
 `;
 
 const Content = styled.div`
@@ -23,6 +24,7 @@ const MainPage: FC = () => {
         <Wrap>
             <TopBar/>
             <Content>
+                <LeftMenu/>
             </Content>
         </Wrap>
     )
